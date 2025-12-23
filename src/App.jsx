f07f0c92c963341fdd3ea5ad433bc3f18620c679
Matt Dennis <matt.dennis@gmail.com>
2025-12-22 19:41:43 -0800

    Change 'delivery date' to 'due date' in warning message

--- src/App.jsx
+++ src/App.jsx
@@ -253,7 +253,7 @@ function App() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
                       <p className="text-xs text-gray leading-relaxed">
-                        Note: This plan extends payment beyond your delivery date.
+                        Note: This plan extends payment beyond your due date.
                       </p>
                     </div>
                   )}
